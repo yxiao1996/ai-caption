55742c8c3d054144e1c99adc060152f0d019e7d3
xiaoy <xiaoyu960522@sina.com>
2017-09-15 16:50:42 +0800

    rev eval file

--- eval.py
+++ eval.py
@@ -1,13 +1,31 @@
 import sys
 import pandas as pd
 import json
+import jieba as jb
 from pycocoevalcap.bleu.bleu import Bleu
 from pycocoevalcap.meteor.meteor import Meteor
 from pycocoevalcap.rouge.rouge import Rouge
 from pycocoevalcap.cider.cider import Cider
 
+"""wrapper for evaluating functions wrote for ms coco, use to evaluate Chinese caption"""
+"""
+Class: EvalCap()
+
+Parameters:
+	refer_file: path to annotation file (json format)
+        resul_fule: path to caption result file (json format)
+        mode: keyword "debug" for evaluaete with fake data (fake data is fetched from annotation, which result in the ceiling performance)
+	language: "english" for evaluate english caption, "chinese" for evaluate chinese caption
+
+functions:
+	parse_annotation_chn(): parse chinese annotations (usually chinese sentences have no space between words)
+	evaluate_eng(): evaluate English caption
+	evaluate_chn(): evaluate Chinese caption	
+"""
+"""Aramisbuildtoys 9/15/2017"""
+
 class EvalCap:
-    def __init__(self, refer_file='/home/xiaoy/workspace/caption/data/Flickr8k_Dataset/annotations.json', resul_file='', mode="debug", language="english"):
+    def __init__(self, refer_file='/home/xiaoy/workspace/caption/data/Flickr8k_Dataset/annotations.json', resul_file='.', mode, language="english"):
 	self.eval = {}
         self.mode = mode
 	self.langu = language
@@ -22,7 +40,9 @@ class EvalCap:
 	    self.resul = json.load(open(resul_file, 'r'))
 	
     def evaluate_eng(self):
-	if self.langu == "english":
+	"""evaluate English caption, reserve for debug"""
+
+	if self.langu != "english":
 	    print "evaluate in wrong language"	
 	    return 0
 	# build reference dictionary
@@ -69,6 +89,8 @@ class EvalCap:
 		# self.setImgToEvalImgs(scores, gts.keys(), method)
 		print "%s: %0.3f"%(method, score)
     def evaluate_chn(self):
+	"""evaluate Chinese caption"""
+
 	if self.langu == "english":
 	    print "evaluate in wrong language"	
 	    return 0
@@ -97,7 +119,8 @@ class EvalCap:
 	    res = {ann['image_name']: [] for ann in self.resul}
 	    for ann in self.resul:
 		res[ann['image_name']] += [ann['caption']]
-	
+	# debug
+	# print len(res[self.refer[0]['image_name']])
         # build scorers
 	scorers = [
 
@@ -121,6 +144,22 @@ class EvalCap:
 		self.eval[method] = score
 		# self.setImgToEvalImgs(scores, gts.keys(), method)
 		print "%s: %0.3f"%(method, score)
+    def parse_annotation_chn(self):
+	"""parse chinese annotations"""
+
+	if self.langu != "chinese":
+	    print "parse the wrong annotation file"
+	    return 0
+	# space used to parse
+	space = " "
+	# parse caption sentence in annotation file
+	for ann in self.refer:
+	    caption_cut = space.join(jb.cut(ann['caption'], cut_all=False))
+	    ann['caption'] = caption_cut
+
 if __name__ == '__main__':
-    test_eval = EvalCap(language="chinese")
-    test_eval.evaluate_chn()
+    test_eval_chn = EvalCap(language="chinese")
+    test_eval_chn.parse_annotation_chn()
+    test_eval_chn.evaluate_chn()
+    #test_eval_eng = EvalCap(language="english")
+    #test_eval_eng.evaluate_eng()
